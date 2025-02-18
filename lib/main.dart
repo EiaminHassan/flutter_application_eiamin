427c480dae17073bb1a2163e19d8a62878dcac05
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-02-18 12:59:15 +0600

    shoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(const LabClass05());
@@ -7,54 +6,73 @@ void main() {
 
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Basic Flutter UI - 02",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic Flutter UI - 02"),
-            backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("Basic Flutter UI - 02"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            elevation: 5,
+            child: Column(
+              children: [
+                // Image in Card
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                  child: Image.asset(
+                    'assets/shoes.jpg',
+                    width: 300,  // Adjust the width of the image
+                    height: 200, // Adjust the height of the image
+                    fit: BoxFit.cover, // Make sure the image fits nicely
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      // Price text
+                      Text(
+                        'Price: \$100',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                      // Discount text
+                      SizedBox(height: 5),
+                      Text(
+                        'Discount: 20% off',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.red,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      // Button or other actions (e.g., Add to Cart)
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add action here
+                        },
+                        child: Text('Add to Cart'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(
-                Iconsax.activity,
-                size: 50,
-                color: Colors.blue,
-              ),
-              Icon(
-                Iconsax.activity,
-                size: 50,
-                color: Colors.black,
-              ),
-              Icon(
-                Iconsax.activity,
-                size: 50,
-                color: Colors.red,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Iconsax.activity,
-                    size: 50,
-                    color: Colors.blue,
-                  ),
-                  Icon(
-                    Iconsax.activity,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Iconsax.activity,
-                    size: 50,
-                    color: Colors.red,
-                  ),
-                ],
-              )
-            ],
-          )),
+        ),
+      ),
     );
   }
 }
